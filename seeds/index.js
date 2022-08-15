eab23332a86072535445a5b078b24c2d1af97986
JeSeungHoo <loiivsy@naver.com>
2022-08-15 17:27:36 +0900

    basic style - ejs-mate, bootstrap5

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30);
         const c = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem molestiae quas distinctio tempora earum officiis soluta tempore facere unde autem sapiente architecto delectus blanditiis eveniet, incidunt dicta quod recusandae at?',
+            price
         });
         await c.save();
     }
